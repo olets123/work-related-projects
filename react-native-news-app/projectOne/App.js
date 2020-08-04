ee1253ae05c89a019f0ff930253d567722d7f0e7
olets123 <47143886+olets123@users.noreply.github.com>
2020-08-04 20:42:54 +0200

    create react-native

---
+++ react-native-news-app/projectOne/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TabScreen from './src/screens/TabScreen.js';
+
+export default class App extends React.Component {
+render() {
+  return (
+    <TabScreen />
+  );
+ }
+}
